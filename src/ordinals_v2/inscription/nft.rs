0e13150361cb1e2651b2cb1ddbec2b5dd453df2a
Kobby Pentangeli <kobbypentangeli@gmail.com>
2024-03-04 04:41:57 +0000

    Refactor `Nft` implementation with tests

--- src/ordinals_v2/inscription/nft.rs
+++ src/ordinals_v2/inscription/nft.rs
@@ -1,47 +1,50 @@
 //! NFT
 
 pub mod id;
+#[cfg(test)]
+mod nft_tests;
 
 use super::constants;
 use crate::{utils, InscriptionParseError, OrdError, OrdResult};
 
 use bitcoin::{
     opcodes,
-    script::{Builder as ScriptBuilder, PushBytesBuf},
+    script::{Builder as ScriptBuilder, PushBytesBuf, ScriptBuf},
 };
+use http::HeaderValue;
 use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
-use std::str::FromStr;
+use std::{io::Cursor, str::FromStr};
 
-/// Represents an arbitrary Ordinal inscription. We're "unofficially" referring to this as an NFT
+/// Represents an arbitrary Ordinal Nft. We're "unofficially" referring to this as an NFT
 /// (e.g., like an ERC721 token).
 ///
-/// Inscriptions may include fields before an optional body.
+/// Nfts may include fields before an optional body.
 /// Each field consists of two data pushes, a tag and a value.
 ///
-/// [Reference](https://docs.ordinals.com/inscriptions.html#fields)
+/// [Reference](https://docs.ordinals.com/Nfts.html#fields)
 #[serde_as]
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 pub struct Nft {
-    /// The main body of the inscription.
+    /// The main body of the Nft.
     pub body: Option<Vec<u8>>,
-    /// Its tag is 1, and its value is the MIME type of the body.
+    /// Has a tag of 1, representing the MIME type of the body.
     pub content_type: Option<Vec<u8>>,
     /// Has a tag of 2, representing the position of the inscribed sat in the outputs.
     pub pointer: Option<Vec<u8>>,
-    /// Has a tag of 3, representing the parent inscription, i.e., the owner of an inscription
-    /// can create child inscriptions.
+    /// Has a tag of 3, representing the parent Nft, i.e., the owner of an Nft
+    /// can create child Nfts.
     pub parent: Option<Vec<u8>>,
     /// Has a tag of 5, representing CBOR metadata, stored as data pushes.
     pub metadata: Option<Vec<u8>>,
-    /// Has a tag of 7, whose value is the metaprotocol identifier.
+    /// Has a tag of 7, representing the metaprotocol identifier.
     pub metaprotocol: Option<Vec<u8>>,
     pub incomplete_field: bool,
     pub duplicate_field: bool,
-    /// Has a tag of 9, whose value represents the encoding of the body.
+    /// Has a tag of 9, representing the encoding of the body.
     pub content_encoding: Option<Vec<u8>>,
     pub unrecognized_even_field: bool,
-    /// Has a tag of 11, representing a nominated inscription.
+    /// Has a tag of 11, representing a nominated Nft.
     pub delegate: Option<Vec<u8>>,
 }
 
@@ -139,32 +142,31 @@ impl Nft {
         utils::bytes_to_push_bytes(self.encode()?.as_bytes())
     }
 
-    /// Returns the NFT inscription's content_type as a string if available, or `None` otherwise.
+    pub fn body(&self) -> Option<&[u8]> {
+        Some(self.body.as_ref()?)
+    }
+
+    pub fn content_length(&self) -> Option<usize> {
+        Some(self.body()?.len())
+    }
+
     pub fn content_type(&self) -> Option<&str> {
         std::str::from_utf8(self.content_type.as_ref()?).ok()
     }
 
-    /// Returns the NFT inscription's body as bytes if available, or `None` otherwise.
-    pub fn body_bytes(&self) -> Option<&[u8]> {
-        Some(self.body.as_ref()?)
+    pub fn content_encoding(&self) -> Option<HeaderValue> {
+        HeaderValue::from_str(
+            std::str::from_utf8(self.content_encoding.as_ref()?).unwrap_or_default(),
+        )
+        .ok()
     }
 
-    /// Returns the NFT inscription's body as a string if available, or `None` otherwise.
-    ///
-    /// # Errors
-    ///
-    /// Returns `Err` if the slice is not UTF-8 with a description as to why the provided slice is not UTF-8.
-    pub fn body_str(&self) -> Option<&str> {
-        std::str::from_utf8(self.body.as_ref()?).ok()
+    pub fn metadata(&self) -> Option<ciborium::Value> {
+        ciborium::from_reader(Cursor::new(self.metadata.as_ref()?)).ok()
     }
 
-    /// Returns the NFT inscription's metadata as a string if available, or `None` otherwise.
-    ///
-    /// # Errors
-    ///
-    /// Returns `Err` if the slice is not UTF-8 with a description as to why the provided slice is not UTF-8.
-    pub fn metadata(&self) -> Option<&str> {
-        std::str::from_utf8(self.metadata.as_ref()?).ok()
+    pub fn metaprotocol(&self) -> Option<&str> {
+        std::str::from_utf8(self.metaprotocol.as_ref()?).ok()
     }
 
     pub fn pointer_value(pointer: u64) -> Vec<u8> {
@@ -175,6 +177,10 @@ impl Nft {
         }
 
         bytes
+    }
+
+    pub fn reveal_script_as_scriptbuf(&self, builder: ScriptBuilder) -> ScriptBuf {
+        self.append_reveal_script_to_builder(builder).into_script()
     }
 }
 
@@ -190,79 +196,151 @@ impl FromStr for Nft {
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn nft_creation() {
-    //     let nft = Nft::new(
-    //         Some(b"text/plain".to_vec()),
-    //         Some(b"Hello, world!".to_vec()),
-    //         None,
-    //     );
-    //     assert_eq!(nft.content_type(), Some("text/plain"));
-    //     assert_eq!(nft.body_str(), Some("Hello, world!"));
-    //     assert!(nft.metadata().is_none());
-    // }
-
-    // #[test]
-    // fn json_serialization_deserialization() {
-    //     let nft = Nft::new(
-    //         Some(b"text/plain".to_vec()),
-    //         Some(b"Hello, world!".to_vec()),
-    //         None,
-    //     );
-    //     let encoded = nft.encode().unwrap();
-    //     let decoded: Nft = Nft::from_json_str(&encoded).unwrap();
-    //     assert_eq!(nft, decoded);
-    // }
-
-    // #[test]
-    // fn to_push_bytes_conversion() {
-    //     let nft = Nft::new(None, Some(b"Hello, world!".to_vec()), None);
-    //     let push_bytes = nft.as_push_bytes().unwrap();
-    //     assert_eq!(push_bytes.as_bytes(), nft.encode().unwrap().as_bytes());
-    // }
-
-    // #[test]
-    // fn invalid_utf8() {
-    //     let invalid_utf8 = vec![0xff, 0xfe, 0xfd];
-    //     let nft = Nft::new(None, Some(invalid_utf8.clone()), Some(invalid_utf8.clone()));
-    //     assert!(nft.body_str().is_none());
-    //     assert!(nft.metadata().is_none());
-    //     assert!(nft.content_type().is_none());
-    // }
+    use nft_tests::create_nft;
 
     #[test]
-    fn invalid_utf8_content_type() {
-        let json = "{\"content_type\": [255, 255, 255]}"; // Invalid UTF-8 bytes
-        let nft = Nft::from_json_str(json);
-        assert!(nft.is_err());
+    fn reveal_script_chunks_body() {
+        assert_eq!(
+            create_nft("btc", [])
+                .reveal_script_as_scriptbuf(ScriptBuilder::new())
+                .instructions()
+                .count(),
+            7
+        );
+
+        assert_eq!(
+            create_nft("btc", [0; 1])
+                .reveal_script_as_scriptbuf(ScriptBuilder::new())
+                .instructions()
+                .count(),
+            8
+        );
+
+        assert_eq!(
+            create_nft("btc", [0; 520])
+                .reveal_script_as_scriptbuf(ScriptBuilder::new())
+                .instructions()
+                .count(),
+            8
+        );
+
+        assert_eq!(
+            create_nft("btc", [0; 521])
+                .reveal_script_as_scriptbuf(ScriptBuilder::new())
+                .instructions()
+                .count(),
+            9
+        );
+
+        assert_eq!(
+            create_nft("btc", [0; 1040])
+                .reveal_script_as_scriptbuf(ScriptBuilder::new())
+                .instructions()
+                .count(),
+            9
+        );
+
+        assert_eq!(
+            create_nft("btc", [0; 1041])
+                .reveal_script_as_scriptbuf(ScriptBuilder::new())
+                .instructions()
+                .count(),
+            10
+        );
     }
 
-    // #[test]
-    // fn test_valid_nft() {
-    //     // Example JSON with `content_type`, `body`, and `metadata` as arrays of byte values
-    //     let json = r#"{
-    //     "content_type": [116, 101, 120, 116, 47, 112, 108, 97, 105, 110],
-    //     "body": [72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100, 33],
-    //     "metadata": [123, 34, 99, 114, 101, 97, 116, 111, 114, 34, 58, 32, 34, 65, 108, 105, 99, 101, 34, 125]
-    // }"#;
+    #[test]
+    fn reveal_script_chunks_metadata() {
+        assert_eq!(
+            Nft {
+                metadata: None,
+                ..Default::default()
+            }
+            .reveal_script_as_scriptbuf(ScriptBuilder::new())
+            .instructions()
+            .count(),
+            4
+        );
 
-    //     let nft = Nft::from_json_str(json).unwrap();
+        assert_eq!(
+            Nft {
+                metadata: Some(Vec::new()),
+                ..Default::default()
+            }
+            .reveal_script_as_scriptbuf(ScriptBuilder::new())
+            .instructions()
+            .count(),
+            4
+        );
 
-    //     // Assuming `content_type` and `metadata` are text, we can convert them to strings for assertion
-    //     assert_eq!(nft.content_type().unwrap(), "text/plain");
-    //     assert_eq!(nft.body_str().unwrap(), "Hello, world!");
-    //     assert_eq!(nft.metadata().unwrap(), "{\"creator\": \"Alice\"}");
-    // }
+        assert_eq!(
+            Nft {
+                metadata: Some(vec![0; 1]),
+                ..Default::default()
+            }
+            .reveal_script_as_scriptbuf(ScriptBuilder::new())
+            .instructions()
+            .count(),
+            6
+        );
+
+        assert_eq!(
+            Nft {
+                metadata: Some(vec![0; 520]),
+                ..Default::default()
+            }
+            .reveal_script_as_scriptbuf(ScriptBuilder::new())
+            .instructions()
+            .count(),
+            6
+        );
+
+        assert_eq!(
+            Nft {
+                metadata: Some(vec![0; 521]),
+                ..Default::default()
+            }
+            .reveal_script_as_scriptbuf(ScriptBuilder::new())
+            .instructions()
+            .count(),
+            8
+        );
+    }
 
     #[test]
-    fn invalid_mime_type_nft() {
-        let json = r#"{
-            "content_type": "plain",
-            "body": "SGVsbG8sIHdvcmxkIQ==",
-            "metadata": "eyJjcmVhdG9yIjogIkFsaWNlIn0="
-        }"#;
+    fn metadata_function_decodes_metadata() {
+        assert_eq!(
+            Nft {
+                metadata: Some(vec![0x44, 0, 1, 2, 3]),
+                ..Default::default()
+            }
+            .metadata()
+            .unwrap(),
+            ciborium::Value::Bytes(vec![0, 1, 2, 3]),
+        );
+    }
 
-        let nft = Nft::from_json_str(json);
-        assert!(nft.is_err());
+    #[test]
+    fn metadata_function_returns_none_if_no_metadata() {
+        assert_eq!(
+            Nft {
+                metadata: None,
+                ..Default::default()
+            }
+            .metadata(),
+            None,
+        );
+    }
+
+    #[test]
+    fn metadata_function_returns_none_if_metadata_fails_to_parse() {
+        assert_eq!(
+            Nft {
+                metadata: Some(vec![0x44]),
+                ..Default::default()
+            }
+            .metadata(),
+            None,
+        );
     }
 }
