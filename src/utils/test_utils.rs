2d9b31502ba31f116412acb89bbe6fbcc7936c5a
Kobby Pentangeli <kobbypentangeli@gmail.com>
2024-03-06 08:30:13 +0000

    Add issue and pull-request templates

--- src/utils/test_utils.rs
+++ src/utils/test_utils.rs
@@ -20,6 +20,36 @@ pub fn generate_btc_address(network: Network) -> (Address, PrivateKey) {
     (address, private_key)
 }
 
+/// Converts a SEC1 ECDSA signature to the DER format.
+#[allow(unused)]
+pub fn sec1_to_der(sec1_signature: Vec<u8>) -> Result<Vec<u8>, String> {
+    if sec1_signature.len() != 64 {
+        return Err("Invalid SEC1 signature length".to_string());
+    }
+
+    let mut r = sec1_signature[..32].to_vec();
+    if r[0] & 0x80 != 0 {
+        r.insert(0, 0x00);
+    }
+
+    let mut s = sec1_signature[32..].to_vec();
+    if s[0] & 0x80 != 0 {
+        s.insert(0, 0x00);
+    }
+
+    let mut der_signature = Vec::with_capacity(6 + r.len() + s.len());
+    der_signature.push(0x30);
+    der_signature.push((4 + r.len() + s.len()) as u8);
+    der_signature.push(0x02);
+    der_signature.push(r.len() as u8);
+    der_signature.extend(r);
+    der_signature.push(0x02);
+    der_signature.push(s.len() as u8);
+    der_signature.extend(s);
+
+    Ok(der_signature)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
--- src/wallet/builder/signer2.rs
+++ src/wallet/builder/signer2.rs
@@ -291,34 +291,3 @@ impl From<bitcoin::ecdsa::Signature> for OrdSignature {
         Self::Ecdsa(sig)
     }
 }
-
-// Converts a SEC1 ECDSA signature to the DER format.
-#[cfg(test)]
-#[allow(unused)]
-pub(crate) fn sec1_to_der(sec1_signature: Vec<u8>) -> Result<Vec<u8>, String> {
-    if sec1_signature.len() != 64 {
-        return Err("Invalid SEC1 signature length".to_string());
-    }
-
-    let mut r = sec1_signature[..32].to_vec();
-    if r[0] & 0x80 != 0 {
-        r.insert(0, 0x00);
-    }
-
-    let mut s = sec1_signature[32..].to_vec();
-    if s[0] & 0x80 != 0 {
-        s.insert(0, 0x00);
-    }
-
-    let mut der_signature = Vec::with_capacity(6 + r.len() + s.len());
-    der_signature.push(0x30);
-    der_signature.push((4 + r.len() + s.len()) as u8);
-    der_signature.push(0x02);
-    der_signature.push(r.len() as u8);
-    der_signature.extend(r);
-    der_signature.push(0x02);
-    der_signature.push(s.len() as u8);
-    der_signature.extend(s);
-
-    Ok(der_signature)
-}
